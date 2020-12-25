41d174057ecc21f0cb3f209ed3ab4448a0210a57
manoharkharvi43 <manoharkharvi43@gmail.com>
2020-12-25 17:25:58 +0530

    ScreenOrientation fixed

--- Components/VideoPlayer.jsx
+++ Components/VideoPlayer.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useEffect ,useState} from "react";
+import { StyleSheet, Text, View, Dimensions ,ActivityIndicator} from "react-native";
 import { Video } from "expo-av";
 import * as ScreenOrientation from "expo-screen-orientation";
 import Constants from "expo-constants";
+import { useRef } from "react";
 
 export default function VideoPlayer({ navigation, route }) {
 	const width = Dimensions.get("window").width;
-	const height = Dimensions.get("window").height;
+    const height = Dimensions.get("window").height;
+    const [loading, setLoading] = useState(false); 
 	const { videourl } = route.params;
-
+    const videoRef = useRef(null)
 	useEffect(() => {
 		toLandscape();
 		return () => {
@@ -25,21 +27,22 @@ export default function VideoPlayer({ navigation, route }) {
 		await ScreenOrientation.lockAsync(
 			ScreenOrientation.OrientationLock.PORTRAIT
 		);
-	};
+    };
+    
+
 	return (
-		<View style={styles.videoContainer}>
+		<View style={styles.videoContainer}>     
 			<Video
-				source={{ uri: videourl }}
+				source={{ uri:videourl}}
 				rate={1.0}
 				volume={1.0}
 				isMuted={false}
 				resizeMode="cover"
 				shouldPlay={false}
 				isLooping={false}
-				usePoster
-				useNativeControls
-				onFullscreenUpdate
-				// onPlaybackStatusUpdate={(AVPlaybackStatus)=>console.log(AVPlaybackStatus.isBuffering())}
+                usePoster
+                ref={videoRef}
+                useNativeControls={true}
 				style={{
 					width: height/1,
 					height: width/1.1,
@@ -53,7 +56,7 @@ const styles = StyleSheet.create({
 	videoContainer: {
 		marginTop: Constants.statusBarHeight,
 		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
+        justifyContent: "center",
+        alignItems:'center'
 	},
 });
